ff814156f00f7792c8c3b57a1f408d15dd4d94f7
matuxer <matias@biott.com.ar>
2022-12-01 17:58:55 -0300

    Hice la ruta DELETE users/:id

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const {
-   get,
+   get, deleteUser,
 } = require('../controllers/users')
 
 const router = express.Router()
 
 router.get('/', get)
 
+router.delete('/:id', deleteUser)
 
 module.exports = router
