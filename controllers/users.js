520fd99c274ad0e07be39410038bb8651801f6fb
jbresky <joaquin_bresky@hotmail.com>
2022-12-02 08:28:13 -0300

    solo formatee el codigo

--- controllers/users.js
+++ controllers/users.js
@@ -2,10 +2,10 @@ const createHttpError = require('http-errors')
 const { User } = require('../database/models')
 const { endpointResponse } = require('../helpers/success')
 const { catchAsync } = require('../helpers/catchAsync')
-const {ErrorObject} = require('../helpers/error.js')
+const { ErrorObject } = require('../helpers/error.js')
 const bcrypt = require('bcryptjs')
 
-const {generateJwt} = require('../helpers/generate-JWT.js')
+const { generateJwt } = require('../helpers/generate-JWT.js')
 
 // example of a controller. First call the service, then build the controller method
 module.exports = {
@@ -25,34 +25,37 @@ module.exports = {
       next(httpError)
     }
   }),
-  create: catchAsync(async(req,res,next)=>{
+  create: catchAsync(async (req, res, next) => {
     try {
-      const {firstName,lastName,email,password,avatar,roleId,deletedAt} = req.body
-      const hashPasword = bcrypt.hashSync(password,8)
-      const [user,created] = await User.findOrCreate({
-        where: {email},
+      const { firstName, lastName, email, password, avatar, roleId, deletedAt } = req.body
+      const hashPasword = bcrypt.hashSync(password, 8)
+
+      const [user, created] = await User.findOrCreate({
+        where: { email },
         defaults: {
           firstName,
           lastName,
           email,
-          password:hashPasword,
+          password: hashPasword,
           avatar,
           roleId,
           deletedAt,
-        }});
+        }
+      });
+      
       //verificar si ya existe el usuario
-      if(!created){
+      if (!created) {
         const error = new ErrorObject("Email already exist", 400)
         throw error
-      }else{
+      } else {
         const token = await generateJwt(user.id)
         console.log("token: ", token)
         endpointResponse({
           res,
           message: 'User created successfully',
-          body:{user, token}
+          body: { user, token }
         })
-      } 
+      }
     } catch (error) {
       const httpError = createHttpError(
         error.statusCode,
