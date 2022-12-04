9bfab6b9421f133dc1692044d592a15ebd86aa05
Matias Biott <matias@biott.com.ar>
2022-12-04 20:50:43 -0300

    Endpoint para datos de categorias (#15)
    * Added the function getId to the categories router
    
    * Created the function getId for the categories controller

--- controllers/categories.js
+++ controllers/categories.js
@@ -16,9 +16,30 @@ module.exports = {
     } catch (error) {
       const httpError = createHttpError(
         error.statusCode,
-        `[Error retrieving users] - [index - GET]: ${error.message}`
+        `[Error retrieving categories] - [index - GET]: ${error.message}`
       );
       next(httpError);
+    }
+  }),
+  getId: catchAsync(async (req, res, next) => {
+    const { id } = req.params
+    try {
+      const response = await Category.findByPk(id)
+      if (response) {
+        endpointResponse({
+          res,
+          message: 'Category retrieved successfully',
+          body: response,
+        })
+      } else {
+        throw new ErrorObject("Can't find the category you're looking for", 400);
+      }
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error retrieving category] - [index - GET]: ${error.message}`,
+      )
+      next(httpError)
     }
   })
 }
