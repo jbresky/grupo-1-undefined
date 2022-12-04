ba0a0e1b368a92922b81d7c04d3d8fb2ab737ecf
matuxer <matias@biott.com.ar>
2022-12-04 13:37:54 -0300

    Created the function editCategory for the categories controller

--- controllers/categories.js
+++ controllers/categories.js
@@ -16,7 +16,31 @@ module.exports = {
     } catch (error) {
       const httpError = createHttpError(
         error.statusCode,
-        `[Error retrieving users] - [index - GET]: ${error.message}`
+        `[Error retrieving categories] - [index - GET]: ${error.message}`
+      );
+      next(httpError);
+    }
+  }),
+  editCategory: catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const category = await Category.findByPk(id);
+    if (!category) throw new ErrorObject("Can't find the category you're looking for", 404);
+    const { name, description } = req.body;
+    try {
+      category.set({
+        name,
+        description
+      });
+      const response = await category.save();
+      endpointResponse({
+        res,
+        message: `${response.name} updated succesfully`,
+        body: response,
+      });
+    } catch (error) {
+      const httpError = createHttpError(
+        error.statusCode,
+        `[Error editing category] - [index - GET]: ${error.message}`
       );
       next(httpError);
     }
