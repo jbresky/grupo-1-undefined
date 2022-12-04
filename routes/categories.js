e41887d816508b0f3d9a0a5581e4f6f62bf9928a
matuxer <matias@biott.com.ar>
2022-12-04 12:59:19 -0300

    Added the function getId to the categories router

--- routes/categories.js
+++ routes/categories.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { get } = require('../controllers/categories');
+const { get, getId } = require('../controllers/categories');
 
 const router = express.Router();
 
 router.get('/', get);
+router.get('/:id', getId);
 
 module.exports = router;
