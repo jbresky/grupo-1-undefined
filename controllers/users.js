e576d830fadf89fdc8bcafd163f3e26cf2167ced
matuxer <matias@biott.com.ar>
2022-12-05 11:18:34 -0300

    Deleted the loginUser funtion from the users controller

--- controllers/users.js
+++ controllers/users.js
@@ -161,38 +161,4 @@ module.exports = {
       }
     }
   ),
-  loginUser: catchAsync(async (req, res, next) => {
-    const { email, password } = req.body
-    const userInfo = await User.findOne({where: { email: email }})
-    if(!userInfo) {
-      return endpointResponse({
-        res,
-        message: `Email and password combination incorrect`,
-        body: { ok: false },
-      });
-    }
-    try {
-      bcrypt.compare(password, userInfo.password).then((match) => {
-        if (match === false) {
-          return endpointResponse({
-            res,
-            message: `Email and password combination incorrect`,
-            body: { ok: false },
-          });
-        } else {
-          endpointResponse({
-            res,
-            message: `User logged in succesfully`,
-            body: {userInfo, ok: true},
-          });
-        }
-      });
-    } catch (error) {
-      const httpError = createHttpError(
-          error.statusCode,
-          `[Error logging users] - [index - PUT]: ${error.message}`
-        );
-        next(httpError);
-    }
-  })
 };
