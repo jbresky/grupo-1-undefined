e7c3c335732f124b5f054812c14c54d052d8e8e7
matuxer <matias@biott.com.ar>
2022-12-05 11:14:55 -0300

    Created the route /auth

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const usersRouter = require('./users')
 const transactionsRouter = require('./transactions')
 const categoriesRouter = require('./categories')
+const authRouter = require('./auth')
 
 const router = express.Router()
 
@@ -9,5 +10,6 @@ const router = express.Router()
 router.use('/users', usersRouter)
 router.use('/transactions', transactionsRouter)
 router.use('/categories', categoriesRouter)
+router.use('/auth', authRouter)
 
 module.exports = router
