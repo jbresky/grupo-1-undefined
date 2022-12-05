bd42a3a8d57f2361c28c928b1687cbf26f77fa85
Matias Biott <matias@biott.com.ar>
2022-12-05 17:28:36 -0300

    Update 20221204001602-create-roles.js

--- database/seeders/20221204001602-create-roles.js
+++ database/seeders/20221204001602-create-roles.js
@@ -14,25 +14,6 @@ module.exports = {
         description: "Usuarios que administran el sitio",
       }),
     ]);
-
-    /* let roles = [
-      {
-        id: 1,
-        name: "Usuario",
-        description: "Usuarios que consumen el sitio",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        id: 2,
-        name: "Administrador",
-        description: "Usuarios que administran el sitio",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ]; */
-
-    await queryInterface.bulkInsert("Roles", roles, {});
   },
 
   down: async (queryInterface, Sequelize) => {
