b69fd6ee0b178505cce4ef48ed0575971d213e45
matuxer <matias@biott.com.ar>
2022-12-06 18:23:42 -0300

    Modified the transactions seeder

--- database/seeders/20221206031948-create-transactions.js
+++ database/seeders/20221206031948-create-transactions.js
@@ -1,32 +1,12 @@
-'use strict';
+const {transactionGenerator} = require('../../helpers/transactionGenerator');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const transaction = [
-      {
-        id: 1,
-        description: "Transaction",
-        amount: 120,
-        userId: 1,
-        categoryId: 1,
-        date: new Date()
-      },
-      {
-        id: 2,
-        description: "Transaction 2",
-        amount: 140,
-        userId: 2,
-        categoryId: 2,
-        date: new Date()
-      },
-    ]
-    await queryInterface.bulkInsert('transactions', transaction, {});
+    const transactions = await transactionGenerator();
+    return Promise.all([...transactions]);
   },
 
   down: async (queryInterface, Sequelize) => {
-
-    await queryInterface.bulkDelete('transactions', null, {});
-
-  }
+    await queryInterface.bulkDelete("transactions", null, {});
+  },
 };
-
---
+++ helpers/transactionGenerator.js
@@ -0,0 +1,30 @@
+const { User, Transaction } = require("../database/models");
+const Sequelize = require("sequelize");
+const { faker } = require('@faker-js/faker');
+
+module.exports = {
+  transactionGenerator: async () => {
+    let transactionArr = [];
+    let i = 0;
+    while (i < 10) {
+      const randUser = await User.findOne({
+        order: [Sequelize.fn("RAND")],
+      });
+      const description = faker.lorem.sentence();
+      const amount = faker.finance.amount();
+      const date = faker.date.between();
+      const rndInt = Math.floor(Math.random() * 2) + 1
+      transactionArr.push(
+        Transaction.create({
+          description: description,
+          amount: amount,
+          userId: randUser.id,
+          categoryId: rndInt,
+          date: date
+        })
+      );
+      i++;
+    }
+    return transactionArr;
+  }
+}
