55b21f2b36b60cdad7fb9b1a13eeae817134d06b
jbresky <joaquin_bresky@hotmail.com>
2022-12-08 14:44:42 -0300

    adding middleware for edit user

--- routes/users.js
+++ routes/users.js
@@ -9,12 +9,13 @@ const {
 
 const { schemaValidator } = require('../middlewares/schemaValidator');
 const { registration } = require('../schemas/registration');
+const { edit } = require('../schemas/registration');
 
 const router = express.Router()
 
 router.get('/', get)
 router.post('/', schemaValidator(registration), create)
-router.put('/:id',schemaValidator(registration), editUser)
+router.put('/:id', schemaValidator(edit), editUser)
 router.get('/:id', getId)
 router.delete('/:id', deleteUser)
 
