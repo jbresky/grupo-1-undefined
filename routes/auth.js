79b78891ce94cb3d58909813f097c275d5739965
jbresky <joaquin_bresky@hotmail.com>
2022-12-14 19:50:15 -0300

    adding /auth/me route

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { loginUser } = require("../controllers/auth");
+const { loginUser, me } = require("../controllers/auth");
+const validateToken = require("../middlewares/token");
 
 const router = express.Router();
 
+router.get('/me', validateToken, me)
 router.post("/login", loginUser);
 
 module.exports = router;
